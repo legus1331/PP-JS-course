f1c86fc69cfc75e355779aa4f2f51bcc419582a0
Legus1331 <legioner005@gmail.com>
2024-04-11 16:40:34 +0600

    step2

--- step 1/main.js
+++ step 1/main.js
@@ -3,6 +3,26 @@
 // console.log('Hello', firstName)
 
 
-let firstName = 'George'
-firstName = 'Ivan'
-console.log('Hello', firstName)
+// let firstName = 'George'
+// firstName = 'Ivan'
+// console.log('Hello', firstName)
+
+const age = 27;
+console.log(typeof age);
+const bigItn = 123456789524n;
+console.log(typeof bigItn);
+const firstName = 'Name';
+console.log(typeof firstName);
+
+let age2;
+console.log(age2);
+
+const obj = {
+    name: 'George',
+    age: 27,
+    isWorked: true
+}
+
+const num = 10;
+const num2 = 5;
+console.log(num + num2);
